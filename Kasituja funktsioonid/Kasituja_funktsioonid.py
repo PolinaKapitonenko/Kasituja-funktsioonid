73fdcf7612f5895a59f12366b41559c87ce7536b
opilane <opilane@HP-CZC2349HTP>
2023-02-10 12:23:06 +0200

    Add project files.

---
+++ Kasituja funktsioonid/Kasituja_funktsioonid.py
@@ -0,0 +1,9 @@
+def arithmetic(a: int, b: int, operator: str) -> None:
+   if operator == '+':
+       print(a+b)
+   elif operator== '-':
+       print(a-b)
+   elif operator== '*':
+       print(a*b)
+   elif operator== '/':
+       print(a/b)
