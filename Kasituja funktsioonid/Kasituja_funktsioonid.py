5671e74009e345d45795e1618b93883832b85e79
opilane <opilane@HP-CZC2349HTP>
2023-02-10 14:01:47 +0200

    kasutaja

--- Kasituja funktsioonid/Kasituja_funktsioonid.py
+++ Kasituja funktsioonid/Kasituja_funktsioonid.py
@@ -1,9 +1,24 @@
-def arithmetic(a: int, b: int, operator: str) -> None:
-   if operator == '+':
-       print(a+b)
-   elif operator== '-':
-       print(a-b)
-   elif operator== '*':
-       print(a*b)
-   elif operator== '/':
-       print(a/b)
+﻿from math import *
+from random import*
+from module1 import*
+#1
+vastus=arithmetic(float(input("arv1:")), input("tehe:"), float(input("arv2:")))
+print(vastus)
+
+
+#2
+vastus=is_year_leap(input("Aasta:"))
+print()
+#või
+print(is_year_leap(input("Aasta: ")))
+#3
+
+#4
+
+#5
+
+#6
+
+#7
+
+#8
---
+++ Kasituja funktsioonid/module1.py
@@ -0,0 +1,20 @@
+﻿def arithmetic (arv1:float,tehe:str,arv2:float)->any:
+    """Haarme arithmetic.
+    :parem int a,b: Järjend arithmetic numbridest
+    irtype: str
+    """
+    if tehe=="+":
+       vastus=arv1+arv2
+    elif tehe=="-":
+       vastus=arv1+arv2
+    elif tehe=="*":
+        vastus=arv1+arv2
+    elif tehe=="/":
+        if arv2==0:
+           vsatus="DIV0"
+        else:
+            vastus=arv1/arv2
+    else:
+        vastus="Tundmatu tehe"
+
+    return vastus
