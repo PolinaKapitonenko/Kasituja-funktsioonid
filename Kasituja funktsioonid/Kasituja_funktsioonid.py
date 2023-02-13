992ba55d86ff4bacc8b88b2d2b4aa837243ea331
PolinaKapitonenko <120027531+PolinaKapitonenko@users.noreply.github.com>
2023-02-13 12:30:11 +0200

    Update Kasituja_funktsioonid.py

--- Kasituja funktsioonid/Kasituja_funktsioonid.py
+++ Kasituja funktsioonid/Kasituja_funktsioonid.py
@@ -1,6 +1,7 @@
-﻿from math import *
+from math import *
 from random import*
 from module1 import*
+#13/02/2023
 #1
 vastus=arithmetic(float(input("arv1:")), input("tehe:"), float(input("arv2:")))
 print(vastus)
@@ -11,14 +12,36 @@ vastus=is_year_leap(input("Aasta:"))
 print()
 #või
 print(is_year_leap(input("Aasta: ")))
+
 #3
+
+
+spin3=square(float(input("Напишите значение  ")))
+print(spin3)
 
 #4
 
+spin4=season(int(input("m: ")))
+print(spin4)
+print()
+print()
+
 #5
+b=bank(float(input("Milline sum?")),int(input("Mitu aastat?")))
+print(b)
 
 #6
 
+spin6=is_prime(int(input("Kirjutage number ")))
+print(spin6)
+print()
+print()
+
 #7
 
+kuupäev=date_(int(input("Kuupäev: "),int(input("Kuu :"),int(input("Aasta: ")))))
+print(kuupäev)
+
 #8
+
+
